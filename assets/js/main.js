c4e0b04a142cf90adf25b7ac7e5a23f974850049
Marcelo Rocha <celoficial@hotmail.com>
2021-08-21 17:24:14 -0300

    testimonial version 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 //about tabs
-
 (() => {
   const aboutSection = document.querySelector(".about-section");
   const tabsContainer = document.querySelector(".about-tabs");
@@ -22,4 +21,52 @@
       aboutSection.querySelector(target).classList.add("active");
     }
   });
+})();
+
+//testimonial slider
+(() => {
+  const sliderContainer = document.querySelector(".testi-slider-container");
+  slides = sliderContainer.querySelectorAll(".testi-item");
+  slideWidth = sliderContainer.offsetWidth;
+  prevBtn = document.querySelector(".testi-slider-nav .prev");
+  nextBtn = document.querySelector(".testi-slider-nav .next");
+  activeSlide = sliderContainer.querySelector(".testi-item.active");
+
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(
+    activeSlide
+  );
+
+  console.log(slideIndex);
+
+  slides.forEach((slide) => {
+    slide.style.width = slideWidth + "px";
+  });
+  sliderContainer.style.width = slideWidth * slides.length + "px";
+
+  nextBtn.addEventListener("click", () => {
+    if (slideIndex === slides.length - 1) {
+      slideIndex = 0;
+    } else {
+      slideIndex++;
+    }
+    slider();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    if (slideIndex === 0) {
+      slideIndex = slides.length - 1;
+    } else {
+      slideIndex--;
+    }
+    slider();
+  });
+
+  function slider() {
+    sliderContainer
+      .querySelector(".testi-item.active")
+      .classList.remove("active");
+    slides[slideIndex].classList.add("active");
+    sliderContainer.style.marginLeft = -(slideWidth * slideIndex) + "px";
+  }
+  slider();
 })();
